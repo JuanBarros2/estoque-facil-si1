81dada39407d438e95609885148ec12eb075a314
José Renan <joserenansl99@gmail.com>
2018-02-25 12:23:28 -0300

    Corrigido problema com método de criar produto e lote

--- src/main/webapp/app/core/services/produto-lote.service.js
+++ src/main/webapp/app/core/services/produto-lote.service.js
@@ -3,7 +3,7 @@ app.factory("ProdutoLoteService", ["$http", "AppConfig", function ($http, AppCon
     const baseServiceUrl = `${AppConfig.baseUrl}/produto`;
 
     function _cria(idProduto, lote) {
-        $http.post(`${baseServiceUrl}/${idProduto}/lote`, produto)
+        return $http.post(`${baseServiceUrl}/${idProduto}/lote`, produto)
             .then(response => {
                 console.log(response)
                 if (response.status === 201) {
